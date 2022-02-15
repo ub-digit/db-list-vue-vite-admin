53eb290c31ceeda515b33ab2b5012a5850f69200
Johan <johan.larsson@ub.gu.se>
2022-02-15 10:38:32 +0100

    add init data for development purpose

--- src/stores/topics.js
+++ src/stores/topics.js
@@ -3,9 +3,50 @@ import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore({
   id: "topics",
-  state: () => ({
-
-  }),
+  state: () => {
+    return {
+        topics:  [
+          {
+            id: 1,
+            title_sv: "Ekonomi",
+            title_en: "Economy",
+            sub_topics: [
+              {
+                id:21, 
+                title_sv: "Ekonomi sub 21",
+                title_en: "Economy sub 21",
+                can_be_removed: false 
+              },
+              {
+                id:22, 
+                title_sv: "Ekonomi sub 22",
+                title_en: "Economy sub 22",
+                can_be_removed: false
+              },
+              {
+                id:23, 
+                title_sv: "Ekonomi sub 23",
+                title_en: "Economy sub 23",
+                can_be_removed: true
+              }
+            ]
+          },
+          {
+            id: 2,
+            title_sv: "Gender studies sv 23",
+            title_en: "Gender studies 23",
+            sub_topics:[]
+          },
+          {
+            id: 3,
+            title_sv: "Teknologi",
+            title_en: "Technology",
+          },
+        ]
+  
+      }
+  
+  },
   getters: {
   },
   actions: {
