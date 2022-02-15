3a258ba899e6173445f114887626f1204f871973
Johan <johan.larsson@ub.gu.se>
2022-02-15 11:50:58 +0100

    adds fontawesome

--- src/main.js
+++ src/main.js
@@ -3,15 +3,10 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {faArrowLeft, faArrowRight, faTimes, faChevronDown, faChevronUp, faPlus, faMinus, faLock, faLockOpen} from "@fortawesome/free-solid-svg-icons";
-import {faSquare, faCheckSquare, faEdit} from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faArrowLeft, faArrowRight, faChevronDown, faChevronUp, faTimes, faPlus, faMinus, faSquare, faCheckSquare, faLock, faLockOpen, faEdit);
+import {useFontawesome} from '@/plugins/fontawesome.js';
 
 const app = createApp(App);
-
+const {FontAwesomeIcon} = useFontawesome();
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
