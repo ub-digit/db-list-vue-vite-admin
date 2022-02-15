7ec45bd3c8b5cb2747cfa282c10c5bf1d08699b6
Johan <johan.larsson@ub.gu.se>
2022-02-15 12:03:43 +0100

    add view for topics route

---
+++ src/views/TopicsView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row subjects">
+    <div v-if="topics.length" class="row">
+      <div class="col-2">
+        <ul class="list-unstyled">
+          <li v-for="topic in topics" :key="topic.id">
+              {{topic.id}}
+            <!--<router-link :to="{ name: 'TopicShow', params: { id: topic.id }}">{{topic.title_en}}</router-link> -->
+          </li>
+        </ul>
+      </div>
+      <div class="col">
+        <router-view></router-view>
+      </div>
+    </div>
+    <div v-else>
+      No topics created
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+import { useTopicsStore } from "@/stores/topics"
+import {computed } from 'vue'
+
+export default {
+  name: 'Topics',
+  setup() {
+    const topicStore = useTopicsStore();
+    return {
+      topics: computed(() => topicStore.topics), 
+    } 
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    margin-bottom: 40px;
+  }
+  ul{
+        border-right: 1px solid #000;
+  }
+  a {
+    text-decoration: none;
+    &.router-link-active {
+      text-decoration: underline;
+    }
+  }
+
+</style>
