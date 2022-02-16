c6e6f3d1367abab37b4bfd2e1144d7e9a38693cb
Johan <johan.larsson@ub.gu.se>
2022-02-16 12:16:47 +0100

    add navbar and header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import Nav from './components/Nav.vue';
 </script>
 
 <template>
+  <header>
+    <Nav/>
+  </header>
+  <div class="container">
   <RouterView />
+  </div>
 </template>
 
 <style lang="scss">
