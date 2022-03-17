43aa8f3a9c1b73f4ec49d89fce7546216a4c2b4a
Johan <johan.larsson@ub.gu.se>
2022-03-17 11:03:42 +0100

    add vue-formulate to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import VueFormulate from '@braid/vue-formulate';
 import App from "./App.vue";
 import router from "./router";
 import {useFontawesome} from '@/plugins/fontawesome.js';
@@ -12,5 +12,5 @@ app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(VueFormulate.default);
 app.mount("#app");
